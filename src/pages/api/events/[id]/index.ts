fe6951a771d63c4d01e7b7861d3629643abc6bb9
Benjamin Steyaert <appleholic@gmail.com>
2024-09-11 16:00:08 -0700

    Working full CRUD

--- src/pages/api/events/[id]/index.ts
+++ src/pages/api/events/[id]/index.ts
@@ -10,9 +10,26 @@ export const PUT: APIRoute = async ({params, request}) => {
 	}
 
 	const event = await request.json();
-	const updatedEvent = await instance.put(id, event);
+	const updatedEvent = await instance.put(`/api/event/update/${id}`, event);
 
 	return new Response(JSON.stringify(updatedEvent.data), {
+		status: 200,
+		headers: {
+			"Content-Type": "application/json"
+		}
+	})
+}
+
+export const GET: APIRoute = async ({params, request}) => {
+	const id = params.id;
+	if(!id) {
+		return new Response(JSON.stringify({err: 'id is required'}), {
+			status: 200
+		})
+	}
+	const singleEvents = await instance.get(`/api/event/${id}`);
+
+	return new Response(JSON.stringify(singleEvents.data), {
 		status: 200,
 		headers: {
 			"Content-Type": "application/json"
